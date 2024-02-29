7a65392a8801c5bed8b3ab307a5f14cade6f5d24
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-02-29 11:32:52 +0200

    add dependencies and structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
