0c939465c7a4aa6ea8f3e852292645ee7f10da6a
Oleg Kovriga <olegkovrigadev@gmail.com>
2024-03-01 04:31:28 +0200

    Update Loader

--- src/components/UI/Loader/Loader.jsx
+++ src/components/UI/Loader/Loader.jsx
@@ -4,7 +4,9 @@ import LinearProgress from '@mui/material/LinearProgress';
 export default function Loader() {
   return (
     <Box sx={{ width: '100%' }}>
-      <LinearProgress />
+      <LinearProgress
+        sx={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
+      />
     </Box>
   );
 }
