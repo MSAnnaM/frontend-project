063bc08b93cf1a4663e50627e7d87704223958c3
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-03-07 16:18:43 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AddCardModal from './AddModal/AddCardModal';
 import EditUserModal from './staticComponents/EditUserModal';
 import { PrivateRoute } from './PrivateRoutes';
 import { PublicRoute } from './PublicRoute';
+import { useDispatch, useSelector } from 'react-redux';
+import { isRefreshing } from '../redux/user/selectors';
+import { refreshUser } from '../redux/user/userApi';
 
 const WelcomePage = lazy(() => import('pages/WelcomePage'));
 const AuthPage = lazy(() => import('pages/AuthPage'));
@@ -19,39 +22,48 @@ const LoginForm = lazy(() => import('components/Auth/LoginForm/LoginForm'));
 const HomePage = lazy(() => import('pages/HomePage'));
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isRefreshed = useSelector(isRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<WelcomePage />}>
-          <Route index element={<WelcomeView />} />
-          {/* Public Route from AuthPage */}
+    !isRefreshed && (
+      <>
+        <Routes>
+          <Route path="/" element={<WelcomePage />}>
+            <Route index element={<WelcomeView />} />
+            {/* Public Route from AuthPage */}
+            <Route
+              path="/auth/*"
+              element={
+                <PublicRoute>
+                  <AuthPage />
+                </PublicRoute>
+              }
+            >
+              <Route path="register" element={<RegisterForm />} />
+
+              <Route path="login" element={<LoginForm />} />
+            </Route>
+            <Route path="edit" element={<EditUserModal />} />
+            <Route path="add" element={<AddCardModal />} />
+          </Route>
+
+          {/* Private Route from HomePage */}
           <Route
-            path="/auth/*"
+            path="/home"
             element={
-              <PublicRoute>
-                <AuthPage />
-              </PublicRoute>
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
             }
           >
-            <Route path="register" element={<RegisterForm />} />
-            <Route path="login" element={<LoginForm />} />
+            {/* <Route path="/home/:boardName" element={<ScreensPage />} /> */}
           </Route>
-          <Route path="edit" element={<EditUserModal />} />
-          <Route path="add" element={<AddCardModal />} />
-        </Route>
-
-        {/* Private Route from HomePage */}
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        >
-          {/* <Route path="/home/:boardName" element={<ScreensPage />} /> */}
-        </Route>
-      </Routes>
-    </>
+        </Routes>
+      </>
+    )
   );
 };
