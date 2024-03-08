95d1c68420aaecbb07ee1b568790d0230ba162e9
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-08 17:21:59 +0200

    formik Modal

--- src/components/ScreensPage/AddColumn/AddColumn.jsx
+++ src/components/ScreensPage/AddColumn/AddColumn.jsx
@@ -4,6 +4,7 @@ import Icon from '../Icon/Icon';
 import css from './AddColumn.module.css';
 import { addColumn } from '../../../redux/column/columnApi';
 import { useShownBoard } from 'hooks/useShownBoard';
+import { Field, Form, Formik } from 'formik';
 
 const AddColumn = ({ closeModal }) => {
   const dispatch = useDispatch();
@@ -28,21 +29,23 @@ const AddColumn = ({ closeModal }) => {
   return (
     <div className={css.container}>
       <p className={css.addColumn_title}>Add column</p>
-      <form onSubmit={handlerAddColumn}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          className={css.addColumn_input}
-          autoFocus
-        />
-        <Button type="submit" className={css.addColumn_btn}>
-          <span className={css.iconWrap}>
-            <Icon id={'icon-plus'} className={css.icon} />
-          </span>
-          Add
-        </Button>
-      </form>
+      <Formik>
+        <Form onSubmit={handlerAddColumn}>
+          <Field
+            type="text"
+            name="title"
+            placeholder="Title"
+            className={css.addColumn_input}
+            autoFocus
+          ></Field>
+          <Button type="submit" className={css.addColumn_btn}>
+            <span className={css.iconWrap}>
+              <Icon id={'icon-plus'} className={css.icon} />
+            </span>
+            Add
+          </Button>
+        </Form>
+      </Formik>
     </div>
   );
 };
