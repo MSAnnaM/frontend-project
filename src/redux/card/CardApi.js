8cb0d00b953e1a6c35cb37a062975212684527e5
AnastasiaGOIT <hudzevych.music@gmail.com>
2024-03-09 20:28:24 +0200

    cardmodals

--- src/redux/card/CardApi.js
+++ src/redux/card/CardApi.js
@@ -4,6 +4,18 @@ export const api = axios.create({
   baseURL: 'http://localhost:3005/api',
 });
 
+export const fetchCards = createAsyncThunk(
+  'cards/fetchCards',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.get('/cards/');
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 export const addCard = createAsyncThunk(
   'cards/addCard',
   async (cardData, { rejectWithValue }) => {
@@ -20,7 +32,7 @@ export const editCard = createAsyncThunk(
   'cards/editCard',
   async (cardData, { rejectWithValue }) => {
     try {
-      const response = await axios.put(`cards/${cardData._id}`, cardData);
+      const response = await axios.put(`cards/${cardData.id}`, cardData);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
