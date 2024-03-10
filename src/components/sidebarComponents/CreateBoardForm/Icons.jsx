4b95ade014c9a2b2859c802d0b0aacba07d6921a
taraspro5 <kovtun_taras007@ukr.net>
2024-03-10 19:08:28 +0200

    create board form icons and backgrounds

---
+++ src/components/sidebarComponents/CreateBoardForm/Icons.jsx
@@ -0,0 +1,14 @@
+import sprite from '../../../img/icons/sprite.svg';
+
+const icons = [
+  { id: 'icon-project', image: `${sprite}#icon-project` },
+  { id: 'icon-star', image: `${sprite}#icon-star` },
+  { id: 'icon-loading', image: `${sprite}#icon-loading` },
+  { id: 'icon-puzzle', image: `${sprite}#icon-puzzle` },
+  { id: 'icon-container', image: `${sprite}#icon-container` },
+  { id: 'icon-lightning', image: `${sprite}#icon-lightning` },
+  { id: 'icon-colors', image: `${sprite}#icon-colors` },
+  { id: 'icon-hexagon', image: `${sprite}#icon-hexagon` },
+];
+
+export default icons;
